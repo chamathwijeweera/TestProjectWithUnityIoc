54b6a6ba5d819e41bde720bc5d9417c1bab1e840
Pragash Rajarathnam <p.rajarathnam@sana-commerce.com>
2019-01-16 15:07:51 +0530

    Indent the code properly

--- TestProjectWithUnityIoc/TestBase.cs
+++ TestProjectWithUnityIoc/TestBase.cs
@@ -28,25 +28,28 @@ namespace TestProjectWithUnityIoc
             Log(expectedXml);
             Log("RESPONSE RESULT");
             Log(resultXml);
+
             var orginalXmlHash = ConvertStringToHash(expectedXml);
             var resultXmlHash = ConvertStringToHash(resultXml);
+
             return (orginalXmlHash == resultXmlHash);
         }
 
         private string ConvertStringToHash(string rawData)
         {
-            // Create a SHA256   
+            // Create a SHA256
             using (SHA256 sha256Hash = SHA256.Create())
             {
-                // ComputeHash - returns byte array  
+                // ComputeHash - returns byte array
                 byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
 
-                // Convert byte array to a string   
+                // Convert byte array to a string
                 StringBuilder builder = new StringBuilder();
                 for (int i = 0; i < bytes.Length; i++)
                 {
                     builder.Append(bytes[i].ToString("x2"));
                 }
+
                 return builder.ToString();
             }
         }
